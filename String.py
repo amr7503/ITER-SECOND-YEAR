60b913fe2f5a5de811f816a22b75fd40a355fff7
AMARESH SWAIN <50235506+amr7503@users.noreply.github.com>
2024-10-03 13:00:20 +0000

    cc 3 oct 24

---
+++ String.py
@@ -0,0 +1,25 @@
+s='Hello world'
+# c=0
+# for ch in s:
+#     c=c+1
+# print("Number of characters = ",c)
+
+# String Slicing
+# print(s[1:4])
+
+# Membership operator
+# print('x' in s)
+
+#Inbuilt functions
+# print(s.count('l')) #Count a specific character's occurrence
+
+# #count the number of vowels in a string
+# v='aeiou'
+# c=0
+# for ch in v:
+#     c=c+s.count(ch)
+# print(c)
+
+# find() , rfind() , capitalize() , lower() , upper() , swapcase()
+
+
