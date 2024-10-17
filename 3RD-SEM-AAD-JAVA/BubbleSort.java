709fce795d48bace926b5b9032f3744bb242b399
AMARESH SWAIN <50235506+amr7503@users.noreply.github.com>
2024-10-17 03:22:15 +0000

    c171024

---
+++ 3RD-SEM-AAD-JAVA/BubbleSort.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class BubbleSort
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number of elements in array");
+        int n = sc.nextInt();
+        int arr[]=new int[n];
+        System.out.print("Enter "+n+" elements : ");
+        for (int i=0;i<n;i++) {
+            arr[i]=sc.nextInt();
+        }
+        //int arr[]={23,55,76,12,54,33};
+
+        for(int i=0;i<n;i++)
+        {
+            boolean sorted=true;
+            for(int j=0;j<n-1-i;j++)
+            {
+              if(arr[j]>arr[j+1])
+               {
+                sorted=false;
+                int t=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=t;
+               }
+           }
+           if(sorted) break;
+        }
+        System.out.println("Array after sorting : ");
+        for (int i=0;i<n;i++)
+        System.err.print(arr[i]+" ");
+    }
+}
---
+++ 3RD-SEM-AAD-JAVA/InsertionSort.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class InsertionSort {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array: ");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        sc.close();
+
+        for(int i=1;i<n;i++)
+        {
+            int x=arr[i];
+            int j=i;
+            while(j>0 && arr[j-1]>x )
+            {
+                arr[j]=arr[j-1];
+                j--;
+            }
+            arr[j]=x;
+
+        }
+
+        System.out.println("The array after sorting is: ");
+        for(int i=0;i<n;i++)
+            System.out.print(arr[i]+" ");
+    }
+}
