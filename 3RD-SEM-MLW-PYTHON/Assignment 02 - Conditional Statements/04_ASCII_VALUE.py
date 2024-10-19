a5101712b0543edf5a3dfbf987674f800b370e18
Amaresh Swain <amaresh7503@gmail.com>
2024-10-19 20:23:32 +0530

    cc191024

--- 3RD-SEM-MLW-PYTHON/Assignment 02 - Conditional Statements/04_ASCII_VALUE.py
+++ 3RD-SEM-MLW-PYTHON/Assignment 02 - Conditional Statements/04_ASCII_VALUE.py
@@ -1,2 +1,10 @@
 ch=input("Enter a character : ")
-print(ord(ch))
+ascii=ord(ch)
+if ascii>=65 and ascii<=90:
+    print("Capital letter")
+elif ascii>=97 and ascii<=122:
+    print("Small case letter")
+elif ascii>=48 and ascii<=57:
+    print("Digit")
+else:
+    print("Special Symbol")
