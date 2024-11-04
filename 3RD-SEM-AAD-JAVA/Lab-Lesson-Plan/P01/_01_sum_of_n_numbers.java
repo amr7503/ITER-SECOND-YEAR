ea23f2febb0573f69eeb7cf1714ec75e8e6dfdf2
Amaresh Swain <amaresh7503@gmail.com>
2024-11-05 00:30:51 +0530

    cc.

---
+++ 3RD-SEM-AAD-JAVA/Lab-Lesson-Plan/P01/_01_sum_of_n_numbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class _01_sum_of_n_numbers {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the value of n : ");
+        int n=sc.nextInt();
+       
+       System.out.println("Enter "+n+" numbers");
+       int s=0;
+       for(int i=1;i<=n;i++)
+        s+=sc.nextInt();
+       System.out.println("Sum of entered numbers is "+s);
+    }
+}
---
+++ 3RD-SEM-AAD-JAVA/Lab-Lesson-Plan/P02/_05_Smallest_Positive_Missing_number.java
@@ -0,0 +1,5 @@
+public class _05_Smallest_Positive_Missing_number {
+    public static void main(String[] args) {
+        System.out.println("HEllo");
+    }
+}
