8b052b798a6906a42969bee92cdced0e52d03fe1
Amaresh Swain <amaresh7503@gmail.com>
2024-11-05 11:40:31 +0530

    .

--- 3RD-SEM-AAD-JAVA/Lab-Lesson-Plan/P04 - Sorting/SelectionSort.java
+++ 3RD-SEM-AAD-JAVA/Lab-Lesson-Plan/P04 - Sorting/SelectionSort.java
@@ -5,7 +5,7 @@ public class SelectionSort {
         for(int i=0;i<n-1;i++)
         {
             int min=i;
-            for(int j=i+1;i<n;i++)
+            for(int j=i+1;j<n;j++)
             {
                 if(arr[j]<arr[min])
                     min=j;
