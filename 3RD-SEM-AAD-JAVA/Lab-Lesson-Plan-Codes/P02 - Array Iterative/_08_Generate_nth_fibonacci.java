dc2b75759633f6e85b40e203a4da99f8f7c92284
Amaresh Swain <amaresh7503@gmail.com>
2025-01-07 13:25:10 +0530

    Ad update 2

--- 3RD-SEM-AAD-JAVA/Lab-Lesson-Plan-Codes/P02 - Array Iterative/_08_Generate_nth_fibonacci.java
+++ 3RD-SEM-AAD-JAVA/Lab-Lesson-Plan-Codes/P02 - Array Iterative/_08_Generate_nth_fibonacci.java
@@ -1,9 +1,22 @@
 import java.util.Scanner;
 
 public class _08_Generate_nth_fibonacci {
-    public static int fib(int n){
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the value of n : ");
+        int n=sc.nextInt();
+        int fib[]=new int[n+1];
+        fib[1]=0;
+        fib[2]=1;
+        for(int i=3;i<=n;i++)
+            fib[i]=fib[i-1]+fib[i-2];
+        System.out.print(n+"th fibonacci is "+ fib[n]);
+    }
+
+    /*  public static int fib(int n){
         if(n==1) return 0;
-        if(n==2) return 2;
+        if(n==2) return 1;
 
         int a=0,b=1,c=1;
         for(int i=3;i<=n;i++)
@@ -14,12 +27,7 @@ public class _08_Generate_nth_fibonacci {
         }
         return c;
     }
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter the value of n : ");
-        int n=sc.nextInt();
-        System.out.print(n+"th fibonacci is "+ fib(n));
-    }
+   */
 }
 
 /*
